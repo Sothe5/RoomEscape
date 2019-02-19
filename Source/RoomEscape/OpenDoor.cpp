29538d93e82b91a0ef5bf685c5344dc61ecfe3dc
sothe5 <almaral4c@gmail.com>
2019-02-19 19:02:01 +0100

    OpenDoor and PositionReport Script Added. Also the room has been modeled

--- Source/RoomEscape/OpenDoor.cpp
+++ Source/RoomEscape/OpenDoor.cpp
@@ -1,36 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	
+
 	// ...
 }
-
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
+	ActorTriggeringPPlate = GetWorld()->GetFirstPlayerController()->GetPawn();
 	// ...
-	FString ObjPos = GetOwner()->GetTransform().GetTranslation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("Position of the %s is %s"), *(GetOwner()->GetName()), *ObjPos);
 	
 }
 
-
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	if (PressurePlate->IsOverlappingActor(ActorTriggeringPPlate)) {
+		openDoor();
+	}
+
 	// ...
 }
 
+void UOpenDoor::openDoor() 
+{
+	AActor* owner = GetOwner();
+
+	owner->SetActorRotation(FRotator(0, -75, 0));
+
+}
